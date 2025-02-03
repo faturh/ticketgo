37d5a7dc4a2f42ea9b6a8d74b1e2bd46324a9172
faturh <fikrihabib25@gmail.com>
2025-02-03 22:55:37 +0700

    update mode;l, fillament

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    // mass assignment
+    protected $fillable = [
+        'name',
+        'slug', // unique karakter untuk mengidentifikasikan untuk developer nanti url seo nya bagus ga angka asal
+        'icon',
+    ];
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
+
