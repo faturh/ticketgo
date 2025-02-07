c6d2a1e87abd9737e0e9e1874f8fa28b383595f3
faturh <fikrihabib25@gmail.com>
2025-02-07 22:49:34 +0700

    update repository

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     //
+    protected $frontService;
+
+    public function __construct(FrontService $frontService)
+    {
+        $this->frontService = $frontService;
+    }
+
+    // konsep service repository pattern
+    public function index()
+    {
+        $data = $this->frontService->getFrontPageData();
+        return view('front.index', $data);
+
+        // konsep mcvp
+    // public function index(){
+    //     $categories = Category::latest()->get();
+    //     $popular_tickets = Ticket::where('is_popular', true)->take(4)->get();
+    //     $new_tickets = Ticket::latest()->get();
+
+    //     return view('front.index', compact('categories', 'popular_tickets', 'new_tickets'));
+    // }
+
 }
